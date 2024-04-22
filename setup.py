73c837a9a2819098694f8a693ed55142b45b87f7
Achyutanand Mishra <achyutanandmishra@Achyutanands-MacBook-Pro.local>
2024-04-22 17:47:22 -0400

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     '''
@@ -16,13 +16,13 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-        return requirements 
+    return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author= 'Anand',
-    author_email='achyutanandmishra17@gmail.com',
-    packages= find_packages(),
-    install_requires= get_requirements('requirement.txt')
+name='mlproject',
+version='0.0.1',
+author='krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirement.txt')
 )
