d9aa9d66e911f3460d6c15934fb7c2bde265febe
Achyutanand Mishra <achyutanandmishra@Achyutanands-MacBook-Pro.local>
2024-07-30 00:39:37 -0400

    Data Ingestion changes

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,3 +1,4 @@
+# src/mlproject/utils.py
 import os
 import sys
 from src.mlproject.exception import CustomException
@@ -6,36 +7,26 @@ import pandas as pd
 from dotenv import load_dotenv
 import pymysql
 
-
 load_dotenv()
 
-host=os.getenv("host")
-user=os.getenv("user")
-password=os.getenv("password")
-db=os.getenv('db')
-
-
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
 
 def read_sql_data():
     logging.info("Reading SQL database started")
     try:
-        mydb=pymysql.connect(
+        mydb = pymysql.connect(
             host=host,
             user=user,
             password=password,
             db=db
         )
-        logging.info("Connection Established",mydb)
-        df=pd.read_sql_query('Select * from seattle_weather',mydb)
+        logging.info("Connection Established")
+        df = pd.read_sql_query('SELECT * FROM students', mydb)
         print(df.head())
 
         return df
-
-
-
     except Exception as ex:
-        raise CustomException(ex)
-    
-
-
-
+        raise CustomException(ex, sys)
